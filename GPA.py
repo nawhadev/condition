97e612bd181e1f9a93aa572c0de145e6ea54c287
nawhadev <86646267+nawhadev@users.noreply.github.com>
2025-08-09 12:51:22 +0800

    Add files via upload

---
+++ GPA.py
@@ -0,0 +1,17 @@
+marks = int(input("Enter your marks:"))
+
+if 80 <= marks <= 100:
+    print("A+")
+elif 70 <= marks <= 79:
+    print("A")
+elif 60 <= marks <= 69:
+    print("A-")
+elif 50 <= marks <= 59:
+    print("B")
+elif 40 <= marks <= 49:
+    print("C")
+elif 33 <= marks <= 39:
+    print("D")
+else:
+    print("F")
+
