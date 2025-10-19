8c660d703996fbabcf7857c5f1fb541edcc07b44
nawhadev <86646267+nawhadev@users.noreply.github.com>
2025-10-19 14:02:07 +0800

    Add files via upload

---
+++ H.W bmi calculater.py
@@ -0,0 +1,14 @@
+weight = float(input("Enter teh weight in kg"))
+height = float(input("Enter the height in metre"))
+bmi = weight/height **2
+print(f"Your BMI is {bmi:0.2f}")
+if bmi <= 18.5:
+    print("You are under weight.Please try again!")
+elif 18.5 <= bmi <= 24.9:
+    print("You are normal.Keep it up!")
+elif 25 <= bmi <= 29.9:
+    print("You are over weight.Please lose some weight")
+elif 30 <= bmi <= 30.4:
+    print("You are obese.Losing weight is emergency!")
+else:
+    print("You are extremely obese.Visit the doctor as soon as possible!")
